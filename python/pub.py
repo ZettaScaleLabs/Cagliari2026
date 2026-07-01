3d9c84044237f86f1b02450156f89bde12abe88e
Michael Ilyin <milyin@gmail.com>
2026-07-01 09:42:07 +0200

    print info

--- python/pub.py
+++ python/pub.py
@@ -8,16 +8,16 @@ config = zenoh.Config().from_file("config_pub.json")
 with zenoh.open(config) as session:
     pub = session.declare_publisher("forkyeah/network/test")
 
-    # info = session.info
-    # print(f"zid: {info.zid()}")
-    # print(f"routers: {info.routers_zid()}")
-    # print(f"peers: {info.peers_zid()}")
-    # print("transports:")
-    # for t in info.transports():
-    #     print(f"  - {t}")
-    # print("links:")
-    # for l in info.links():
-    #     print(f"  - {l}")
+    info = session.info
+    print(f"zid: {info.zid()}")
+    print(f"routers: {info.routers_zid()}")
+    print(f"peers: {info.peers_zid()}")
+    print("transports:")
+    for t in info.transports():
+        print(f"  - {t}")
+    print("links:")
+    for l in info.links():
+        print(f"  - {l}")
 
     for i in range(100):
         print(f"Sent Hello #{i}")
--- python/sub.py
+++ python/sub.py
@@ -1,6 +1,18 @@
 import time
 import zenoh
 
+def print_info(session):
+    info = session.info
+    print(f"zid: {info.zid()}")
+    print(f"routers: {info.routers_zid()}")
+    print(f"peers: {info.peers_zid()}")
+    print("transports:")
+    for t in info.transports():
+        print(f"  - {t}")
+    print("links:")
+    for l in info.links():
+        print(f"  - {l}")
+
 def listener(sample):
     print(f"{sample.key_expr}: {sample.payload.to_string()}")
 
@@ -10,18 +22,10 @@ config = zenoh.Config().from_file("config_sub.json")
 
 with zenoh.open(config) as session:
 
-    # info = session.info
-    # print(f"zid: {info.zid()}")
-    # print(f"routers: {info.routers_zid()}")
-    # print(f"peers: {info.peers_zid()}")
-    # print("transports:")
-    # for t in info.transports():
-    #     print(f"  - {t}")
-    # print("links:")
-    # for l in info.links():
-    #     print(f"  - {l}")
+    print_info(session)
     
     session.declare_subscriber("forkyeah/network/test", listener)
     print("Listening on 'forkyeah/network/test'...")
     while True:
-        time.sleep(1)
+        time.sleep(5)
+        print_info(session)
